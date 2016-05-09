bbed144cda0bb6e0bf4b084806ba8388a4ddf7cc
looping42 <loopingr42@gmail.com>
2016-05-09 14:36:57 +0200

    k

---
+++ Test/Program.cs
@@ -0,0 +1,38 @@
+﻿using Framework.DirectoryMethod;
+using Framework.Img;
+using Framework.Log;
+using Framework.Mail;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Image temp = Img.resizeImageFromFile("C:\\Users\\Denis\\Desktop\\testimg\\test1.jpg", 100, 200);
+            Logger.Info("hello");
+            Logger.Info("hello2");
+
+            Logger.Info("hello3");
+            Logger.Info("hello");
+            Logger.Info("hello2");
+
+            Logger.Info("hello3");
+            Logger.Info("hello");
+            Logger.Info("hello2");
+
+            //Trace.TraceInformation("Test message.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ testweb/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Framework.Img;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace testweb.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public virtual ActionResult Index()
+        {
+            //ViewBag.Image = Img.resizeImageFromFile("E:\\projetvisualstudio\\UnitTest\testweb\\Content\\test1.jpg", 100, 200);
+            //WebImage img = new WebImage("~/Content/test1.jpg").Resize(100, 200).Write();
+
+            //img.Save("~/Content/test2.jpg");
+            return View();
+        }
+
+        //public WebImage Image()
+        //{
+        //    //ViewBag.Image = Img.resizeImageFromFile("E:\\projetvisualstudio\\UnitTest\testweb\\Content\\test1.jpg", 100, 200);
+        //    return Img.resizeImageFromFile("Content/test1.jpg", 400, 200);
+        //    //return new WebImage("~/Content/test1.jpg").Resize(600, 800).Write();
+
+        //    //img.Save("~/Content/test2.jpg");
+        //}
+
+        //public WebImage Image2()
+        //{
+        //    //ViewBag.Image = Img.resizeImageFromFile("E:\\projetvisualstudio\\UnitTest\testweb\\Content\\test1.jpg", 100, 200);
+        //    return Img.resizeImageFromFile("Content/test1.jpg", 400, 200, true);
+        //    //return new WebImage("~/Content/test1.jpg").Resize(600, 800).Write();
+
+        //    //img.Save("~/Content/test2.jpg");
+        //}
+
+        //public WebImage Image3()
+        //{
+        //    //ViewBag.Image = Img.resizeImageFromFile("E:\\projetvisualstudio\\UnitTest\testweb\\Content\\test1.jpg", 100, 200);
+        //    return Img.resizeImageFromFile("Content/test1.jpg", 400, 200);
+        //    //return new WebImage("~/Content/test1.jpg").Resize(600, 800).Write();
+
+        //    //img.Save("~/Content/test2.jpg");
+        //}
+
+        //public virtual ActionResult GetModifiedImage()
+        //{
+        //    var imagePath = Img.resizeImageFromFile("~/Content/test1.jpg", 500, 800);
+
+        //    return base.File(imagePath, "image/jpg");
+        //}
+
+        public virtual ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public virtual ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
